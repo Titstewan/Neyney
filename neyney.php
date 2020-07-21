c75e9ca8b86d0a95b96f1900899040009cc1b52a
Tìtstewan <titstewan@learnnavi.org>
2020-07-21 10:32:20 +0200

    fixed code typo

--- neyney.php
+++ neyney.php
@@ -32,7 +32,7 @@
 	}
 	});
 	// ]'.']></script>
-	<a href="', $scripturl, '?topic=27573" title="Neytiri"><img id="neytiri_stalker" src="', $settings['images_url'], '/' .$context['theme_variant_url'], '/neyney.png" style="border: none; position: fixed; bottom: -40px; right: -15px;"/></a>';
+	<a href="', $scripturl, '?topic=27573" title="Neytiri"><img id="neytiri_stalker" src="', $settings['images_url'], '/', $context['theme_variant_url'], '/neyney.png" style="border: none; position: fixed; bottom: -40px; right: -15px;"/></a>';
 
 	// The minecraft thing
 	if ($context['current_board'] == 199)
@@ -41,5 +41,5 @@
 	#ssssss {position: fixed; bottom: -216px; right: -133px;}
 	#ssssss:hover {bottom: -156px; right: -93px;}
 	</style>
-	<a id="ssssss" href="', $scripturl, '?board=199" title="LearnNavi Minecraft"><img src="', $settings['images_url'], '/' .$context['theme_variant_url'], 'creeper.png"></a>';
+	<a id="ssssss" href="', $scripturl, '?board=199" title="LearnNavi Minecraft"><img src="', $settings['images_url'], '/', $context['theme_variant_url'], 'creeper.png"></a>';
 ?>
